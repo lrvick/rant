9857122caeee24c11b05b2f12385513afee54ddd
lrvick <lance@lrvick.net>
2011-10-08 19:38:21 -0400

    basic template rendering

--- rant/generator.py
+++ rant/generator.py
@@ -1,4 +1,13 @@
+import os
+import yaml
+from jinja2 import Environment, FileSystemLoader
 
 def generate():
+    config_file = file('%s/config.yml' % os.getcwd())
+    config = yaml.load(config_file)
+    print config
+    env = Environment(loader=FileSystemLoader('%s/layouts/' % os.getcwd()))
+    template = env.get_template('base.html')
+    print template.render(config=config)
     print "Site Generated"
     pass

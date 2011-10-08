2016896eed44ca6de5f150de282186c487070358
lrvick <lance@lrvick.net>
2011-10-08 13:09:37 -0400

    current date & time in new publish editing

--- defaults/layouts/publish.md
+++ defaults/layouts/publish.md
@@ -2,7 +2,7 @@
 layout: post
 title:
 date:
-tags:
+tags: []
 comments: true
 draft: false
 ---
--- rant/publisher.py
+++ rant/publisher.py
@@ -1,18 +1,24 @@
 import os
 import tempfile
 import yaml
+import datetime
+from time import gmtime, strftime
 
 def publish(layout='post'):
-    template = '%s/defaults/layouts/publish.md' % os.path.dirname(os.getcwd())
+    template = '%s/rant/defaults/layouts/publish.md' % os.path.dirname(os.getcwd())
     local_template = '%s/layouts/publish.md' % os.getcwd()
     if os.path.isfile(local_template):
         template = local_template
     template_fh = file(template, 'r')
-    header = template_fh.read()
     temp_fh = tempfile.NamedTemporaryFile(delete=False)
-    temp_fh.write(header)
+    line = template_fh.readline()
+    timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    while line:
+        temp_fh.write(line)
+        line = template_fh.readline()
+        if 'date' in line:
+            line = "date: %s\n" % timestamp
     temp_fh.close()
-    #print header
     os.system('vim %s' % temp_fh.name)
     temp_fh = file(temp_fh.name, 'r')
     content = yaml.load_all(temp_fh)

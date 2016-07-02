33dea2b455a01ad83ec7983142bcc084a5d72fb1
Lance R. Vick <lance@lrvick.net>
2016-07-02 02:54:18 -0700

    install defaults, but to wrong folder

--- setup.py
+++ setup.py
@@ -1,21 +1,35 @@
 from setuptools import setup
-import os
 
 setup(
     name='rant',
-    version='0.2.2',
+    version='0.2.3',
     author='Lance R. Vick',
     author_email='lance@lrvick.net',
     packages=['rant'],
     scripts=['bin/rant'],
     url='http://github.com/lrvick/rant',
     license='LICENSE.md',
-    description='A CLI driven blog-aware website generator. It is intended to make maintianing a blog or personal website easy using only your favorite console editor such as Vim, and a few simple commands',
+    description='''
+    A CLI driven blog-aware website generator. It is intended to make
+    maintaining a blog or personal website easy using only your favorite
+    console editor such as Vim, and a few simple commands'
+    ''',
     long_description=open('README.md').read(),
-        #('', ['README.md','LICENSE.md','CHANGES.md','AUTHORS.md']),
-    data_files=[('', ['README.md','LICENSE.md','CHANGES.md','AUTHORS.md'])] +
-        [(d, [os.path.join(d,f) for f in files])
-        for d, folders, files in os.walk('defaults')],
+    package_data={
+        '': [
+            '../README.md',
+            '../LICENSE.md',
+            '../CHANGES.md',
+            '../AUTHORS.md',
+            '../defaults/config.yml',
+            '../defaults/*/*'
+        ]
+    },
+    tests_require=[
+        'flake8',
+        'wheel',
+        'tox',
+    ],
     install_requires=[
         'setuptools == 20.10.1',
         'Jinja2 == 2.8',
